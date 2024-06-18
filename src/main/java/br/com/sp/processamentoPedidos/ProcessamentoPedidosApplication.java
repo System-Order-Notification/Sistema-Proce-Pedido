5e64443ccf6ea55c14c73c97aa42ead1a6387389
AndrewAscar742 <andrewmatosascar@outlook.com>
2024-06-18 14:35:35 -0300

    build: initial commit

---
+++ src/main/java/br/com/sp/processamentoPedidos/ProcessamentoPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.sp.processamentoPedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessamentoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessamentoPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sp/processamentoPedidos/ProcessamentoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sp.processamentoPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessamentoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
